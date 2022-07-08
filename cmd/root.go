4084a530f5062208d69e14c1a783f8429de5daaf
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2022-07-08 18:09:53 +0200

    Added homedir

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,8 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 var cfgFile string
@@ -28,15 +30,10 @@ var cfgFile string
 var RootCmd = &cobra.Command{
 	Use:   "basic_cli_cobra",
 	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Long:  `Get random dad jokes in your terminal`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -44,7 +41,7 @@ to quickly create a Cobra application.`,
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
-		os.Exit(-1)
+		os.Exit(1)
 	}
 }
 
@@ -65,11 +62,20 @@ func init() {
 func initConfig() {
 	if cfgFile != "" { // enable ability to specify config file via flag
 		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.SetConfigName(".basic_cli_cobra") // name of config file (without extension)
+		viper.AddConfigPath(home)               // adding home directory as first search path
+
 	}
 
-	viper.SetConfigName(".basic_cli_cobra") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nfuhs/basic_cli_cobra
 go 1.15
 
 require (
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
 )
